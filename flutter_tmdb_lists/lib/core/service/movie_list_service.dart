4e43f7b7ecc6e0eeea9ba1b611a8a5b38c149fed
Pedro <diaz.gaped24@triana.salesianos.edu>
2026-02-06 10:52:34 +0100

    subida

--- flutter_tmdb_lists/lib/core/service/movie_list_service.dart
+++ flutter_tmdb_lists/lib/core/service/movie_list_service.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
+import 'dart:io';
 
-import 'package:flutter_tmdb_lists/Security/dio_client.dart';
 import 'package:flutter_tmdb_lists/core/interface/movie_list_interfaces.dart';
 import 'package:flutter_tmdb_lists/core/models/movie_list_response.dart';
 import 'package:http/http.dart' as http;
@@ -8,12 +8,15 @@ import 'package:http/http.dart' as http;
 class MovieListService  implements MovieListInterfaces{
   @override
   Future<List<Movie>> getList() async {
-    final DioClient _dioClient = DioClient();
     try {
-      var response = await _dioClient.dio.get('/movie/popular');
+      var response = await http.get(Uri.parse("https://api.themoviedb.org/3/movie/popular"),
+      headers: {
+        "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1YjBjYmY0MDlkMGM0Mjk1YzU5NjMyZDNjYjVlMzRiZCIsIm5iZiI6MTc2MzQ2NTk5OS41NTIsInN1YiI6IjY5MWM1YjBmODUwNTNiZmQxOTVlMDRkMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.lGJ6hvIFhgDjjRTMo9022xIOpGRZXr3BRpz7Zrnkt0s"
+      }
+      );
       
-      if (response.statusCode != null && response.statusCode! >= 200 && response.statusCode! < 300) {
-        var movieList = MovieListResponse.fromJson(response.data).results;
+      if (response.statusCode >= 200 && response.statusCode < 300) {
+        var movieList = MovieListResponse.fromJson(jsonDecode(response.body)).results;
         return movieList;
       } else {
         throw Exception("Error: Status ${response.statusCode}");
